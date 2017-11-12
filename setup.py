ed00a487c0ee29e7e2ccaa671da61e148c328e1d
Andre de Cavaignac <laholmes@hotmail.co.uk>
2017-11-12 16:34:41 +0000

    update pypi setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='risk-tools',
-    packages=['risk-tools'],
-    version='0.1.03',
-    description='risk management metrics library',
-    url='https://github.com/laholmes/risk-tools',
+    name='fin-tools',
+    packages=['fin-tools'],
+    version='0.1.01',
+    description='common financial tools for risk and pricing',
+    url='https://github.com/laholmes/fin-tools',
     author='laurence holmes',
     author_email='laholmes@hotmail.co.uk'
 )
